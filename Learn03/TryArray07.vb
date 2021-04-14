d27b0812b489fdf1acc05d9edddf2f260e1e4b24
Oda Yuhei <64565682+Oda-Yuhei@users.noreply.github.com>
2021-04-14 21:43:23 +0900

    Update TryArray07.vb

--- Learn03/TryArray07.vb
+++ Learn03/TryArray07.vb
@@ -2,24 +2,24 @@ Module TryArray07
 	Sub main()
 		Dim numbers() As Integer
 		numbers = New Integer(){50, 28, 27, 55, 12, 23, 99, 38, -5, 0}
-		Dim sort() As Integer
-		sort = New Integer(UBound(numbers)){}
-		
-		
-		For j As Integer = UBound(numbers) To 0 step -1
-			Dim max As Integer = numbers(j)
-			For i As Integer = 0 To UBound(numbers) - j
-				If numbers(i) > max
-					max = numbers(i)
-				End If
-				sort(j) = max
-			Next
-		Next
-		
-		For Each sor As Integer In sort
-			Console.WriteLine(sor)
-		Next
-		
-		
+        
+        For i As Integer = 0 To UBound(numbers)
+            Dim min As Integer = i
+            For j As Integer = i + 1 To UBound(numbers)
+                If numbers(min) > numbers(j) Then
+                    min = j
+                End If
+                Dim x As Integer  = numbers(i)
+
+                numbers(i) = numbers(min)
+                numbers(min) = x
+                
+            Next j
+        Next i
+
+        For Each num As Integer In numbers
+            Console.WriteLine(num)
+        Next
+        
 	End Sub
 End Module
